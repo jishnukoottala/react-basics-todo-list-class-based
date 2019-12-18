ee51ed9ea2ad552e09d8f6aef338cbb67732cbb6
Jishnu Koottala <jishnu@cloudthing.com>
2019-12-18 14:09:13 +0530

    Updates

---
+++ precache-manifest.eac72ed84598a63daeb3a47734325490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db955f8e718069aadbb6b8837ca7187",
+    "url": "/react-basics-todo-list-class-based/index.html"
+  },
+  {
+    "revision": "b7a0acb430f74e13f41a",
+    "url": "/react-basics-todo-list-class-based/static/css/main.3fb315ec.chunk.css"
+  },
+  {
+    "revision": "9a7d947b2226c7e53ff4",
+    "url": "/react-basics-todo-list-class-based/static/js/2.46595bb0.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/react-basics-todo-list-class-based/static/js/2.46595bb0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7a0acb430f74e13f41a",
+    "url": "/react-basics-todo-list-class-based/static/js/main.3ad0357a.chunk.js"
+  },
+  {
+    "revision": "f0ac22213376b8bf0159",
+    "url": "/react-basics-todo-list-class-based/static/js/runtime-main.b0ff4236.js"
+  }
+]);
